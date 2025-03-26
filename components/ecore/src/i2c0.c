bab50f7269b5bb5fc2ff3f99d36b25996dcc545b
hjt <hjt894399@gmail.com>
2025-03-26 23:04:32 +0800

    IIC

--- components/ecore/src/i2c0.c
+++ components/ecore/src/i2c0.c
@@ -28,21 +28,29 @@ esp_err_t i2c0_init(void)
     return ESP_OK;
 }
 
-
-esp_err_t i2c0_write( uint8_t dev_addr, uint8_t reg_addr, uint8_t *data, uint16_t len)
+esp_err_t i2c0_write(uint8_t dev_addr, uint8_t reg_addr, uint8_t *data, uint16_t len)
 {
-    //后续补充步骤和错误检测
-    
     i2c_cmd_handle_t i2c_cmd = i2c_cmd_link_create();
-    i2c_master_start(i2c_cmd);
-    i2c_master_write_byte(i2c_cmd, (dev_addr << 1 | 0x00), true);
-    i2c_master_write_byte(i2c_cmd, reg_addr, true);
-    for(int i = 0; i < len; i++)
+    if (i2c_master_start(i2c_cmd) == ESP_FAIL)
+        return ESP_FAIL;
+
+    if (i2c_master_write_byte(i2c_cmd, (dev_addr << 1 | 0x00), true) == ESP_FAIL)
+        return ESP_FAIL;
+
+    if (i2c_master_write_byte(i2c_cmd, reg_addr, true) == ESP_FAIL)
+        return ESP_FAIL;
+
+    for (int i = 0; i < len; i++)
     {
-        i2c_master_write_byte(i2c_cmd, data[i], true);
+        if (i2c_master_write_byte(i2c_cmd, data[i], true) == ESP_FAIL)
+            return ESP_FAIL;
     }
-    i2c_master_stop(i2c_cmd);
-    i2c_master_cmd_begin(I2C_NUM_0, i2c_cmd, portMAX_DELAY);
+    if (i2c_master_stop(i2c_cmd) == ESP_FAIL)
+        return ESP_FAIL;
+
+    if (i2c_master_cmd_begin(I2C_NUM_0, i2c_cmd, portMAX_DELAY) == ESP_FAIL)
+        return ESP_FAIL;
+
     i2c_cmd_link_delete(i2c_cmd);
 
     return ESP_OK;
@@ -50,5 +58,29 @@ esp_err_t i2c0_write( uint8_t dev_addr, uint8_t reg_addr, uint8_t *data, uint16_
 
 esp_err_t i2c0_read(uint8_t dev_addr, uint8_t reg_addr, uint8_t *data, uint16_t len)
 {
+    i2c_cmd_handle_t i2c_cmd = i2c_cmd_link_create();
+    if (i2c_master_start(i2c_cmd) == ESP_FAIL)
+        return ESP_FAIL;
+    if (i2c_master_write_byte(i2c_cmd, (dev_addr << 1 | 0x00), true) == ESP_FAIL)
+        return ESP_FAIL;
+    if (i2c_master_write_byte(i2c_cmd, reg_addr, true) == ESP_FAIL)
+        return ESP_FAIL;
+    if (i2c_master_start(i2c_cmd) == ESP_FAIL)
+        return ESP_FAIL;
+    if (i2c_master_write_byte(i2c_cmd, (dev_addr << 1 | 0x01), true) == ESP_FAIL)
+        return ESP_FAIL;
+    for (int i = 0; i < len; i++)
+    {
+        if (i2c_master_read_byte(i2c_cmd, data[i], I2C_MASTER_NACK) == ESP_FAIL)
+            return ESP_FAIL;
+    }
+    if (i2c_master_stop(i2c_cmd) == ESP_FAIL)
+        return ESP_FAIL;
+
+    if (i2c_master_cmd_begin(I2C_NUM_0, i2c_cmd, portMAX_DELAY) == ESP_FAIL)
+        return ESP_FAIL;
+
+    i2c_cmd_link_delete(i2c_cmd);
+
     return ESP_OK;
 }
